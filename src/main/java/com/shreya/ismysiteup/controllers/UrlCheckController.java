c6a16ecd156a8b23445b3cf02d2c3e145e7baa3d
shreyabhardwaj1057 <shreyabhardwaj1057@gmail.com>
2021-01-30 00:25:43 +0530

    added UrlCheckController

---
+++ src/main/java/com/shreya/ismysiteup/controllers/UrlCheckController.java
@@ -0,0 +1,40 @@
+package com.shreya.ismysiteup.controllers;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UrlCheckController {
+    private final String SITE_IS_UP = "Site is up!";
+    private final String SITE_IS_DOWN = "Site is down!";
+    private final String INCORRECT_URL = "Url is Incorrect!";
+
+    @GetMapping("/check")
+    public String getUrlStatusMessage(@RequestParam String url) {
+        String returnMessage = " ";
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int responseCodeCategory = conn.getResponseCode() / 100;
+            if (responseCodeCategory != 2 || responseCodeCategory != 3) {
+                returnMessage = SITE_IS_DOWN;
+            } else {
+                returnMessage = SITE_IS_UP;
+            }
+
+        } catch (MalformedURLException e) {
+            returnMessage = INCORRECT_URL;
+        } catch (IOException e) {
+            returnMessage = SITE_IS_DOWN;
+        }
+        return returnMessage;
+    }
+}
