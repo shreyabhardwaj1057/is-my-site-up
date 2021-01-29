63d67b8e018980aae1b9c2f2c455b97588706c68
shreyabhardwaj1057 <shreyabhardwaj1057@gmail.com>
2021-01-29 19:49:53 +0530

    first commit

---
+++ src/main/java/com/shreya/ismysiteup/IsMySiteUpApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.ismysiteup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsMySiteUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsMySiteUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shreya/ismysiteup/IsMySiteUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreya.ismysiteup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsMySiteUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
